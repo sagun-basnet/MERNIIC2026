ec5dd71d48717f61ae05209f4007741cbb3af0e3
sagun basnet <basnetsagun8@gmail.com>
2026-06-22 12:50:49 +0545

    DOM

--- javascript/main.js
+++ javascript/main.js
@@ -53,14 +53,21 @@
 //   console.log(a);
 // } while (a < 10);
 
-function add(a, b) {
-  if (b === 0) {
-    return;
-  }
-  let res = a / b;
-  return res;
-}
-// console.log(add(1, 1));
+// function add(a, b) {
+//   if (b === 0) {
+//     return;
+//   }
+//   let res = a / b;
+//   return res;
+// }
+
+// function temp() {
+//   return 10;
+// }
+
+// console.log(add(1, temp));
+
+// const add = (a, b) => a + b;
 
 // const result = add(1, 1);
 
@@ -127,5 +134,20 @@ let arr1 = [1, 2, 3, 4, 5, 6];
 let arr2 = [7, 8, 9, 10, 11, 12];
 
 let arr3 = arr1;
-arr3 = arr2;
+// console.log(arr3.length);
+let init = arr3.length;
+// console.log(init);
+
+let condition = arr3.length + arr2.length;
+// console.log(condition);
+
+let count = 0;
+
+for (let i = init; i < condition; i++) {
+  arr3[i] = arr2[count];
+  count++;
+}
 console.log(arr3);
+
+// arr3 = arr2;
+// console.log(arr3);
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -2,5 +2,17 @@ let arr1 = [1, 2, 3, 4, 5, 6];
 let arr2 = [7, 8, 9, 10, 11, 12];
 
 let arr3 = arr1;
-arr3 = arr2;
+// console.log(arr3.length);
+let init = arr3.length;
+// console.log(init);
+
+let condition = arr3.length + arr2.length;
+// console.log(condition);
+
+let count = 0;
+
+for (let i = init; i < condition; i++) {
+  arr3[i] = arr2[count];
+  count++;
+}
 console.log(arr3);
