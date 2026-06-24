9d01b259a3304f486c87e6c73ec9ef5c05df0fd8
sagun basnet <basnetsagun8@gmail.com>
2026-06-24 12:32:00 +0545

    basic js

--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,18 +1,12 @@
-let arr1 = [1, 2, 3, 4, 5, 6];
-let arr2 = [7, 8, 9, 10, 11, 12];
 
-let arr3 = arr1;
-// console.log(arr3.length);
-let init = arr3.length;
-// console.log(init);
+const fetchAPI = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-let condition = arr3.length + arr2.length;
-// console.log(condition);
-
-let count = 0;
-
-for (let i = init; i < condition; i++) {
-  arr3[i] = arr2[count];
-  count++;
-}
-console.log(arr3);
+fetchAPI()
