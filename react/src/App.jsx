0afb60ef704a7465c3e054936e16085544bbe950
sagun basnet <basnetsagun8@gmail.com>
2026-06-28 13:05:53 +0545

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1 className="underline">Hello</h1>
+      <span></span>
+      <p></p>
+    </>
+  );
+};
+export default App;
