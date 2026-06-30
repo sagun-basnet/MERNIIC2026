bd77a04e8e9b49539d85b3bb56566d9b7ac2a476
sagun basnet <basnetsagun8@gmail.com>
2026-06-30 12:44:00 +0545

    useState

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,32 +1,49 @@
+import { useState } from "react";
+import Form from "./components/Form";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import Button from "./components/Button";
 
 const App = () => {
-  const name = "Jhon";
+  // const [count, setCount] = useState(0);
+  // const handleAdd = () => {
+  //   setCount((prev) => prev + 1);
+  //   setCount((prev) => prev + 1);
+  //   // setCount(() => count + 1);
+  //   // setCount(() => {
+  //   //   count + 1;
+  //   // });
+  //   console.log(count);
+  // };
 
-  const handleClick = (id) => {};
-
+  const [isDisplay, setIsDisplay] = useState(false);
+  const handleShow = () => {
+    setIsDisplay(true);
+  };
   return (
     <>
-      <Navbar userName={name} />
-      <Navbar userName={name} />
-
-      <Form text={"Register"}/>
-      {/* <button
-        style={{ backgroundColor: "red" }}
-        onClick={() => {
-          alert("Button Clicked: ", 12);
-        }}
-        className="bg-blue-500 hover:bg-blue-600 hover:text-[1rem]"
-      >
-        Click
-      </button>
-      <h1 className="underline">Hello</h1>
-      <span></span>
-      <p></p> */}
-      {/* <HomePage /> */}
+      <div className="flex flex-col gap-2 justify-center items-center">
+        {/* <span>{count}</span>
+        <button onClick={handleAdd} className="bg-blue-500">
+          Add
+        </button> */}
+        <div className="flex gap-4">
+          <button onClick={handleShow} className="bg-blue-500">
+            {/* {isDisplay ? "ON" : "OFF"} */}
+          </button>
+          <Button text="Show" color="red" />
+          <Button text="Show" color="yellow" />
+          {/* <button onClick={() => setIsDisplay(false)} className="bg-red-500">
+            Hide
+          </button> */}
+        </div>
+        {isDisplay && (
+          <div
+            className={`${isDisplay ? "bg-white" : "bg-black"} h-[80vh] w-full`}
+          ></div>
+        )}
+      </div>
     </>
   );
-  ``;
 };
 export default App;
